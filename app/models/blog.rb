e653db2b3442c41a84d12dc5e8f370c353527475
Suneil Basu <suneil@futureprogress.com>
2016-04-30 19:55:59 +0100

    working on the name scope

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,8 @@
 class Blog < ActiveRecord::Base
   belongs_to :user
 
-  enum flag: [:unread, :underreview, :rejected, :redraft, :published]
   scope :flaged, -> {where(:flag => '0')}
-  scope :status_check, -> (flag){where(:flag => flag)}
+  scope :status_check, -> (flag) {where flag: flag}
 
   def self.search(search)
     where("user_id LIKE ?","%#{search}")
